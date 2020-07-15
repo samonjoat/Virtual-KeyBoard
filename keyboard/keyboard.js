7496b5eeae58b61938f2d9992c965200844f2dd1
Samuel Odira <52766763+czsamzzy@users.noreply.github.com>
2020-07-15 17:57:12 +0100

    js update

---
+++ keyboard/keyboard.js
@@ -0,0 +1,25 @@
+const keyboard = {
+  elements: {
+    main: null,
+    keysContainer: null,
+    keys: [],
+  },
+
+  eventHandlers: {
+    oninput: null,
+    onclose: null,
+  },
+
+  properties: {
+    value: "",
+    capsLock: false,
+  },
+
+  init() {},
+
+  _createKeys() {},
+
+  _triggerEvent(handlerName) {
+      console.log('Event Triggered! Event Name: ')
+  }
+};
